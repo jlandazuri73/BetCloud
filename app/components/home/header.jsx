7f3db61281c0f4a24d9d85771b89c34e2c19a2a8
JuanCode <104174433+jlandazuri73@users.noreply.github.com>
2023-09-11 22:51:50 -0500

    Cambios

--- app/components/home/header.jsx
+++ app/components/home/header.jsx
@@ -1,3 +1,4 @@
+import { Link } from "@remix-run/react";
 import { NAME_APP } from "../../utils/info";
 
 export default function Header() {
@@ -49,12 +50,11 @@ export default function Header() {
             >
               Ingresa
             </a>
-            <a
-              href="#"
+            <Link to={"/accounts/register"}
               className="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
             >
               Comienza
-            </a>
+            </Link>
           </div>
         </div>
       </section>
---
+++ app/routes/accounts/register.jsx
@@ -0,0 +1,46 @@
+import { redirect } from "@remix-run/node";
+import RegisterComponent from "../../components/accounts/register";
+import { isLogin, signup } from "../../data/accounts.server";
+import { validateRegister } from "../../data/validation/accounts";
+
+export default function Register() {
+  return <RegisterComponent />;
+}
+
+export async function action({ request }) {
+  const formData = Object.fromEntries(await request.formData());
+
+  if (request.method === "POST") {
+    console.log(formData, "#");
+    // Validaciones
+    try {
+      validateRegister(formData);
+    } catch (error) {
+      return error;
+    }
+
+
+
+
+    /*
+    // Creando cuenta
+    try {
+      return await signup(formData);
+    } catch (error) {
+      if (error.status === 422) {
+        return { message: error.message };
+      }
+    }
+    */
+  }
+
+  return null;
+}
+
+export async function loader({ request }) {
+  const userIsLogin = await isLogin(request);
+  if (userIsLogin) {
+    return redirect("/app/");
+  }
+  return null;
+}
